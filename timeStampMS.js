34f92cb21c97d2c3e0694de3fd501c35f70aa3c3
Raymond <raymond.rizzo@gmail.com>
2017-04-05 15:46:01 -0400

    Final build

--- timeStampMS.js
+++ timeStampMS.js
@@ -5,11 +5,25 @@ Raymond Rizzo
 
 var http = require('http');
 var express = require('express');
+var url = require('url');
 var app = express();
+var moment = require('moment');
 
-app.get('/hello', function(httpRequest, httpResponse){
-	httpResponse.writeHead(200, {'Content-Type': 'text/html'});
-	httpResponse.end('<h1>Hello World!</h1>');
-		});
+app.get('*', function(httpRequest, httpResponse){
+	var timeObject = {'unix': 0, 'natural': 0};
+	if(httpRequest.url.match(/[A-Z]{0,}[a-z]{0,}%20\d{1,2},%20\d{4}/)){	
+		var naturalDate = httpRequest.url.match(/([A-Z]{0,}[a-z]{0,})%20(\d{1,2}),%20(\d{4})/);
+		timeObject.unix = moment(naturalDate[1] + naturalDate[2] + naturalDate[3],'MMMMDYYYY')/1000;
+		timeObject.natural = moment(timeObject.unix, 'X').format('MMMM D, YYYY');
+		httpResponse.writeHead(200, { "Content-Type": "application/json" });
+		httpResponse.end(JSON.stringify(timeObject));
+	} else if(httpRequest.url.match(/\d{4,}/)) {
+		var unixTime = httpRequest.url.match(/(\d{4,})/);
+		timeObject.unix = unixTime[1];
+		timeObject.natural = moment(timeObject.unix, 'X').format('MMMM D, YYYY');
+		httpResponse.writeHead(200, { "Content-Type": "application/json" });
+		httpResponse.end(JSON.stringify(timeObject));
+	}
+});
 
-app.listen(8080);
+app.listen(80);
